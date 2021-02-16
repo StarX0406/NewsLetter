d08a8da33dfb0033b8f34bc6bd3f9786f660e0e0
Akash Kumar Singh <67006219+StarX0406@users.noreply.github.com>
2021-02-16 17:46:45 +0530

    first-1 commit

---
+++ app.js
@@ -0,0 +1,67 @@
+const express = require("express");
+const request = require("request");
+const bodyParser = require("body-parser");
+const https = require("https");
+const client = require("@mailchimp/mailchimp_marketing"); // you need to add dependency first. See tips.
+ 
+const app = express();
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+ 
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/signUp.html");
+});
+ 
+client.setConfig({
+  apiKey: "d08b36437af851183a649a2be9d755c6-us1",
+  server: "us1",
+});
+ 
+app.post("/", function(req, res) {
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+  console.log(firstName, lastName, email);
+  const subscribingUser = {
+    firstName: firstName,
+    lastName: lastName,
+    email: email
+  }
+ 
+  const run = async () => {
+    try {
+      const response = await client.lists.addListMember("48bd4098a9", {
+        email_address: subscribingUser.email,
+        status: "subscribed",
+        merge_fields: {
+          FNAME: subscribingUser.firstName,
+          LNAME: subscribingUser.lastName
+        }
+      });
+      console.log(response);
+      res.sendFile(__dirname + "/success.html");
+    } catch (err) {
+      console.log(err.status);
+      res.sendFile(__dirname + "/failure.html");
+    }
+  };
+ 
+  run();
+});
+ 
+app.post("/failure", function(req, res) {
+  res.redirect("/");
+});
+ 
+app.listen(process.env.PORT || 3000 , function() {
+  console.log("Server is running on port 3000.");
+});
+// link
+// https://us1.api.mailchimp.com/3.0/list/[list ID]
+
+// API Key
+// d08b36437af851183a649a2be9d755c6-us1
+// list ID
+// 48bd4098a9
